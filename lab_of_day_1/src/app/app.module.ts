ede2c15d0629017ce276fa1ade67d30eb377cf77
Gerges-Hana <83248914+Gerges-Hana@users.noreply.github.com>
2023-02-06 19:05:59 +0200

    day_1

---
+++ lab_of_day_1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IntroSecComponent } from './intro-sec/intro-sec.component';
+import { AppSkillsComponent } from './app-skills/app-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroSecComponent,
+    AppSkillsComponent,
+    PortfolioComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
